37c276acc88eb119ab2ecea16aeb512c37edc738
Mattias Lambert <einzige2@proton.me>
2025-03-07 16:02:09 -0500

    init

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+use crate::mesh_gen::{TerrainResolution, generate_terrain};
+use bevy::prelude::*;
+
+pub mod mesh_gen;
+
+pub fn run() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, input_handler)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    let terrain_size = 32.0;
+
+    let custom_texture_handle: Handle<Image> = asset_server.load("textures/uv_checked.png");
+    let terrain_mesh_handle: Handle<Mesh> =
+        meshes.add(generate_terrain(terrain_size, TerrainResolution::MEDIUM));
+
+    commands.spawn((
+        Mesh3d(terrain_mesh_handle),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color_texture: Some(custom_texture_handle),
+            ..default()
+        })),
+        Transform::from_xyz(-(terrain_size / 2.0), 0.0, -(terrain_size / 2.0)),
+    ));
+
+    let camera_light_transform =
+        Transform::from_xyz(48.0, 48.0, 48.0).looking_at(Vec3::ZERO, Vec3::Y);
+
+    commands.spawn((Camera3d::default(), camera_light_transform));
+    commands.spawn((
+        PointLight {
+            range: 100.0,
+            ..default()
+        },
+        camera_light_transform,
+    ));
+}
+
+fn input_handler(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Transform, With<Camera3d>>,
+    time: Res<Time>,
+) {
+    let mut camera_transform = query.single_mut();
+    let forward = camera_transform.forward();
+    let left = camera_transform.left();
+
+    let movement_speed = 7.5;
+
+    // MOVE FORWARD
+    if keyboard_input.pressed(KeyCode::KeyW) || keyboard_input.pressed(KeyCode::ArrowUp) {
+        camera_transform.translation += forward * time.delta().as_secs_f32() * movement_speed;
+    }
+
+    // MOVE LEFT
+    if keyboard_input.pressed(KeyCode::KeyA) || keyboard_input.pressed(KeyCode::ArrowLeft) {
+        camera_transform.translation += left * time.delta().as_secs_f32() * movement_speed;
+    }
+
+    // MOVE DOWN
+    if keyboard_input.pressed(KeyCode::KeyS) || keyboard_input.pressed(KeyCode::ArrowDown) {
+        camera_transform.translation += -forward * time.delta().as_secs_f32() * movement_speed;
+    }
+
+    // MOVE RIGHT
+    if keyboard_input.pressed(KeyCode::KeyD) || keyboard_input.pressed(KeyCode::ArrowRight) {
+        camera_transform.translation += -left * time.delta().as_secs_f33() * movement_speed;
+    }
+}
---
+++ src/mesh_gen.rs
@@ -0,0 +1,101 @@
+use bevy::{
+    prelude::*,
+    render::{mesh::Indices, render_asset::RenderAssetUsages, render_resource::PrimitiveTopology},
+};
+use noise::{NoiseFn, Perlin};
+
+pub enum TerrainResolution {
+    LOW = 256,
+    MEDIUM = 512,
+    HIGH = 1024,
+}
+
+const SEED: u32 = 42;
+
+fn get_terrain_height(
+    perlin: Perlin,
+    x: f64,
+    z: f64,
+    size: f64,
+    frequency: f64,
+    amplitude: f64,
+) -> f64 {
+    let fx: f64 = x / size * frequency;
+    let fy: f64 = z / size * frequency;
+
+    perlin.get([fx, fy]) * amplitude
+}
+
+pub fn generate_terrain(size: f32, resolution: TerrainResolution) -> Mesh {
+    let res: u32 = resolution as u32;
+    let step: f32 = size as f32 / res as f32;
+
+    // VERTICES
+
+    let perlin: Perlin = Perlin::new(SEED);
+    let mut vertices: Vec<[f32; 3]> = Vec::new();
+    for z in 0..res - 1 {
+        for x in 0..res - 1 {
+            let vx = x as f32 * step;
+            let vz = z as f32 * step;
+            vertices.push([
+                vx,
+                get_terrain_height(perlin, vx as f64, vz as f64, size as f64, 4.0, 5.0) as f32,
+                vz,
+            ]);
+        }
+    }
+
+    // UVS
+
+    let uvs: Vec<[f32; 2]> = vertices
+        .iter()
+        .map(|v| [v[0] / size, v[2] / size])
+        .collect();
+
+    // TRIANGLES
+
+    let mut triangles: Vec<u32> = Vec::new();
+    for n in 0..((res - 2) * (res - 2)) {
+        // each iteration of this loop will be one quad in the overall terrain
+
+        // first triangle
+        // 1    2
+        // ------
+        // |   /
+        // |  /
+        // | /
+        // |/
+        // 0
+
+        triangles.push(n);
+        triangles.push(n + res);
+        triangles.push(n + res + 1);
+
+        // second triangle
+        //      1
+        //     /|
+        //    / |
+        //   /  |
+        //  /   |
+        // ------
+        // 0    2
+
+        triangles.push(n);
+        triangles.push(n + res + 1);
+        triangles.push(n + 1);
+    }
+
+    let mut terrain = Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
+    )
+    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
+    .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, uvs)
+    .with_inserted_indices(Indices::U32(triangles));
+
+    // auto calculate the normals
+    terrain.compute_smooth_normals();
+
+    terrain
+}
